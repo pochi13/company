748628d4b6958f47a8d698f958b51c563647fade
pochi13 <pochi.com777@gmail.com>
2021-08-20 15:35:39 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, # この行にカンマを追加
+    controllers: { registrations: 'registrations' } # この行を追加
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'tweets#index'
    resources :users, only: [:show]
