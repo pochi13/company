c0bb4df794e84527c1dd932daaf24fce15e000e9
pochi13 <pochi.com777@gmail.com>
2021-08-24 19:26:43 +0900

    Revert "basic認証"
    This reverts commit eaa041a20169561d285dcf2e8cae49219753ecd0.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -7,15 +6,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :gender])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :self_introduction, :profile_image])
-  end
-    
-    private
 
-    def basic_auth
-      authenticate_or_request_with_http_basic do |username, password|
-        username == 'admin' && password == '2222'
-      end
-    end
   
-  
+  end
 end
